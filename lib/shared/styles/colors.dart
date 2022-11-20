66e47c511f5641d68e7729eda7fef8c2c56eac6d
nazzaladiy <zalnahanifa28@gmail.com>
2022-11-20 22:11:42 +0700

    final project

---
+++ lib/shared/styles/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Appcolors {
+  static const Color black = Color(0xFF323649);
+  static const Color white = Colors.white;
+  static const Color bleu = Color(0xFF49C9EB);
+  static const Color yellow = Color(0xFFFFA944);
+  static const Color blue = Color(0xFF8DD6D6);
+  static const Color pink = Color(0xFFF65A8A);
+}
